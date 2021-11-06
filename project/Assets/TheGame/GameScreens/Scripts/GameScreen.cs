c63d0eb0b87605bb461be08cf59685c45901ac30
Sergey Bugay <SeregaAltmer@ya.ru>
2021-11-06 16:02:13 +0300

    feat: реализовать заготовку точки входа, GameController-а и экранов игры
    Смена экранов игры настроена через аниматор.
    Для всех предполагаемых экранов игры настроены заглушки.

---
+++ project/Assets/TheGame/GameScreens/Scripts/GameScreen.cs
@@ -0,0 +1,32 @@
+﻿using TheGame.Core.Animations.Attributes;
+using UnityEngine;
+
+namespace TheGame.GameScreens
+{
+  public class GameScreen : AnimatorStateAttributeBehaviour
+  {
+    [SerializeField] private GameObject _prefab;
+    [SerializeField] private bool _mustUnload;
+
+    private GameObject _instance;
+    private GameObject Instance => _instance ? _instance : _instance = MakeInstance();
+
+    protected override void OnActivate() =>
+      Instance.SetActive(true);
+
+    protected override void OnDeactivate()
+    {
+      Instance.SetActive(false);
+      if (!_mustUnload) return;
+
+      Destroy(_instance);
+    }
+
+    private GameObject MakeInstance()
+    {
+      GameObject instance = Instantiate(_prefab, transform, true);
+      instance.name = nameof(Instance);
+      return instance;
+    }
+  }
+}
