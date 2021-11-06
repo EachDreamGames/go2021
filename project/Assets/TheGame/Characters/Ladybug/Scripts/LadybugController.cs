57e02b5c9acd94d1141f728daf83847931c54554
Sergey Bugay <SeregaAltmer@ya.ru>
2021-11-06 22:11:39 +0300

    feat: создать префаб персонажа игрока
    В качестве спрайта персонажа пока используется примитив.
    Добавлен аниматор. Настроено перемещение по горизонтали.

---
+++ project/Assets/TheGame/Characters/Ladybug/Scripts/LadybugController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using TheGame.Core.Animations.Parameters;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace TheGame.Characters.Ladybug
+{
+  public class LadybugController : MonoBehaviour, InputControls.ILadybugActions
+  {
+    [SerializeField] private Animator _animator;
+    [SerializeField] private IntAnimatorParameter _movementDirectionParameter;
+    [SerializeField] private FloatAnimatorParameter _movementSpeedParameter;
+    [SerializeField] private float _movementSpeed;
+
+    private InputControls _inputControls;
+
+    private void Awake()
+    {
+      _inputControls = new InputControls();
+      _inputControls.Ladybug.SetCallbacks(this);
+    }
+
+    private void OnEnable() =>
+      _inputControls.Enable();
+
+    private void OnDisable() =>
+      _inputControls.Disable();
+
+    private void Start() =>
+      _animator.SetValue(_movementSpeedParameter, _movementSpeed);
+
+    public void OnWalk(InputAction.CallbackContext context)
+    {
+      switch (context.phase)
+      {
+        case InputActionPhase.Started:
+        case InputActionPhase.Performed:
+          _animator.SetValue(_movementDirectionParameter, (int)context.ReadValue<float>());
+          break;
+        case InputActionPhase.Disabled:
+        case InputActionPhase.Waiting:
+        case InputActionPhase.Canceled:
+          _animator.SetValue(_movementDirectionParameter, 0);
+          break;
+        default:
+          throw new ArgumentOutOfRangeException();
+      }
+    }
+  }
+}
---
+++ project/Assets/TheGame/Characters/Ladybug/Scripts/LadybugMovement.cs
@@ -0,0 +1,19 @@
+﻿using TheGame.Core.Animations.Attributes;
+using TheGame.Core.Animations.Parameters;
+using UnityEngine;
+
+namespace TheGame.Characters.Ladybug
+{
+  public class LadybugMovement : AnimatorStateAttributeBehaviour
+  {
+    [SerializeField] private Rigidbody2D _body;
+    [SerializeField] private IntAnimatorParameter _direction;
+    [SerializeField] private FloatAnimatorParameter _speed;
+
+    protected override void OnDeactivate() =>
+      _body.velocity = Vector2.zero;
+
+    protected override void OnStateUpdate() =>
+      _body.velocity = new Vector2(Animator.GetValue(_speed) * Animator.GetValue(_direction), _body.velocity.y);
+  }
+}
