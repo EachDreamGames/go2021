ed3d70bda9f6bc982b080bc2ab3a579912c9c92a
Sergey Bugay <SeregaAltmer@ya.ru>
2021-11-09 18:41:46 +0300

    feat: реализовать загрузку и выгрузку стартового уровня
    Пока с хардкодом и статиками, чтобы в принципе появилась возможность
    работать с загрузкой и выгрузкой из главного меню.

---
+++ project/Assets/TheGame/Management/GameController/Scripts/LevelLoad.cs
@@ -0,0 +1,30 @@
+using TheGame.Core.Animations.Attributes;
+using TheGame.Core.Animations.Parameters;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TheGame.Management.GameController.Scripts
+{
+  public class LevelLoad : AnimatorStateAttributeBehaviour
+  {
+    [SerializeField] private BoolAnimatorParameter _isLevelLoaded;
+
+    private const string FirstLevelScene = "TheGame/Levels/Scenes/Level-0";
+
+    public static Scene LoadedScene;
+
+    private void Start() =>
+      SceneManager.sceneLoaded += LevelLoaded;
+
+    protected override void OnStateActivate() =>
+      SceneManager.LoadSceneAsync(FirstLevelScene, LoadSceneMode.Additive);
+
+    private void LevelLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+      if (!IsStateActive) return;
+
+      Animator.SetValue(_isLevelLoaded, true);
+      LoadedScene = scene;
+    }
+  }
+}
---
+++ project/Assets/TheGame/Management/GameController/Scripts/LevelUnload.cs
@@ -0,0 +1,25 @@
+﻿using TheGame.Core.Animations.Attributes;
+using TheGame.Core.Animations.Parameters;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TheGame.Management.GameController.Scripts
+{
+  public class LevelUnload : AnimatorStateAttributeBehaviour
+  {
+    [SerializeField] private BoolAnimatorParameter _isLevelLoaded;
+
+    private void Start() =>
+      SceneManager.sceneUnloaded += LevelUnloaded;
+
+    protected override void OnStateActivate() =>
+      SceneManager.UnloadSceneAsync(LevelLoad.LoadedScene);
+
+    private void LevelUnloaded(Scene scene)
+    {
+      if (!IsStateActive) return;
+
+      Animator.SetValue(_isLevelLoaded, false);
+    }
+  }
+}
