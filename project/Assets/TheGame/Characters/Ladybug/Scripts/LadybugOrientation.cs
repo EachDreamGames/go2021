27a38a6095546832c95f1bf5cc6699941a6170d8
Sergey Bugay <SeregaAltmer@ya.ru>
2021-11-11 20:38:02 +0300

    feat: реализовать поворот персонажа в сторону движения
    Визуально персонаж не поворачивается. Так и задумано.
    Но его игровой объект поворачивается. И так тоже задумано.

---
+++ project/Assets/TheGame/Characters/Ladybug/Scripts/LadybugOrientation.cs
@@ -0,0 +1,34 @@
+﻿using TheGame.Core.Animations.Attributes;
+using TheGame.Core.Animations.Parameters;
+using UnityEngine;
+
+namespace TheGame.Characters.Ladybug
+{
+  public class LadybugOrientation : AnimatorStateAttributeBehaviour
+  {
+    [SerializeField] private Transform _rootTransform;
+    [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private IntAnimatorParameter _orientationParameter;
+
+    private int _prevOrientation;
+
+    protected override void OnStateActivate() =>
+      UpdateOrientation();
+
+    protected override void OnStateUpdate() =>
+      UpdateOrientation();
+
+    private void UpdateOrientation()
+    {
+      int orientation = Animator.GetValue(_orientationParameter);
+      if (_prevOrientation == orientation) return;
+
+      Vector3 localScale = _rootTransform.localScale;
+      localScale.x = orientation < 0 ? -1 : orientation > 0 ? 1 : localScale.x;
+      _rootTransform.localScale = localScale;
+      _renderer.flipX = localScale.x < 0;
+
+      _prevOrientation = orientation;
+    }
+  }
+}
