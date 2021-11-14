227aceebd0e0b04013b5514e2d3b1baccb621cbb
Sergey Bugay <SeregaAltmer@ya.ru>
2021-11-14 13:05:08 +0300

    feat: реализовать ограничение прыжков в воздухе

---
+++ project/Assets/TheGame/Characters/Ladybug/Scripts/LadybugAirJumpCounter.cs
@@ -0,0 +1,28 @@
+﻿using TheGame.Core.Animations.Attributes;
+using TheGame.Core.Animations.Parameters;
+using UnityEngine;
+
+namespace TheGame.Characters.Ladybug
+{
+  public class LadybugAirJumpCounter : AnimatorStateAttributeBehaviour
+  {
+    [SerializeField] private IntAnimatorParameter _availableCountParameter;
+    [SerializeField] private BoolAnimatorParameter _isGroundedParameter;
+    [SerializeField] private int _maxCount;
+
+    private void Start() =>
+      ResetCount();
+
+    protected override void FixedUpdate()
+    {
+      if (Animator.GetValue(_isGroundedParameter))
+        ResetCount();
+    }
+
+    protected override void OnStateActivate() =>
+      Animator.SetValue(_availableCountParameter, Animator.GetValue(_availableCountParameter) - 1);
+
+    public void ResetCount() =>
+      Animator.SetValue(_availableCountParameter, _maxCount);
+  }
+}
