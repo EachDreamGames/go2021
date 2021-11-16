264c3ef3cb3381ac9a1540f036bd276519c8d596
Sergey Bugay <SeregaAltmer@ya.ru>
2021-11-16 19:05:13 +0300

    feat: добавить базовую версию второго уровня
    Настроено поведение камеры при движении по стволу дерева вверх.
    Настроен переход снаружи ствола внутрь, через двери.
    Все использованные спрайты для уровня пока условные.

---
+++ project/Assets/TheGame/Environment/Tree/Scripts/TreeBranch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace TheGame.Environment.Tree.Scripts
+{
+  public class TreeBranch : MonoBehaviour
+  {
+    [SerializeField] private Collider2D _branchCollider;
+    [SerializeField] private LayerMask _trunkLayer;
+    [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private BoolUnityEvent _onVisibilityChanged;
+
+    private void Start() =>
+      UpdateVisibility();
+
+    private void OnTriggerEnter2D(Collider2D other) =>
+      UpdateVisibility();
+
+    private void OnTriggerExit2D(Collider2D other) =>
+      UpdateVisibility();
+
+    private void UpdateVisibility()
+    {
+      _renderer.enabled = _branchCollider.IsTouchingLayers(_trunkLayer);
+      _onVisibilityChanged?.Invoke(_renderer.enabled);
+    }
+
+    [Serializable]
+    private class BoolUnityEvent : UnityEvent<bool>
+    {
+    }
+  }
+}
