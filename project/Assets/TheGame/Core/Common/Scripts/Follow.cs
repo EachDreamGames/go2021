4fd089d9f9d77eb0ef638693d893278d95bdc364
Sergey Bugay <SeregaAltmer@ya.ru>
2021-11-16 18:58:17 +0300

    feat: реализовать компонент следования за целью
    У него не особо интуитивная логика. Он не перемещает свой
    gameobject к цели, а высчитывает дистанцию до неё и удерживает
    gameobject на этой дистанции.

---
+++ project/Assets/TheGame/Core/Common/Scripts/Follow.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace TheGame.Core.Common
+{
+  public class Follow : MonoBehaviour
+  {
+    [SerializeField] private Transform _target;
+    [SerializeField] private bool _ignoreX;
+    [SerializeField] private bool _ignoreY;
+    [SerializeField] private bool _ignoreZ;
+
+    private Vector3 _offset;
+    private Transform _transform;
+
+    private void Start()
+    {
+      _transform = transform;
+      _offset = _target.position - _transform.position;
+    }
+
+    private void Update()
+    {
+      Vector3 currentPosition = _transform.position;
+      Vector3 targetPosition = _target.position;
+      Vector3 newPosition = new Vector3
+      {
+        x = _ignoreX ? currentPosition.x : targetPosition.x - _offset.x,
+        y = _ignoreY ? currentPosition.y : targetPosition.y - _offset.y,
+        z = _ignoreZ ? currentPosition.z : targetPosition.z - _offset.z,
+      };
+
+      _transform.position = newPosition;
+    }
+  }
+}
