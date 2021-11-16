c8e895168452c306b5c50ac127ba71157b0eeee2
Sergey Bugay <SeregaAltmer@ya.ru>
2021-11-16 19:01:59 +0300

    feat: реализовать компонент меняющий режим маски спрайта при старте
    Потребуется для упрощения создания уровня.
    Маска будет использоваться для скрытия части локации второго уровня.
    Но если включить маску во время редактирования то и не видно будет
    уровень пока его редактируешь. А этот компонент сам включит указанный
    режим во время игры.

---
+++ project/Assets/TheGame/Core/Common/Scripts/MaskInteractionActivator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace TheGame.Core.Common
+{
+  public class MaskInteractionActivator : MonoBehaviour
+  {
+    [SerializeField] private Renderer _renderer;
+    [SerializeField] private SpriteMaskInteraction _interaction;
+
+    private void Start()
+    {
+      switch (_renderer)
+      {
+        case TilemapRenderer tilemapRenderer:
+          tilemapRenderer.maskInteraction = _interaction;
+          break;
+        case SpriteRenderer spriteRenderer:
+          spriteRenderer.maskInteraction = _interaction;
+          break;
+      }
+    }
+  }
+}
