a301113ba9c6f524acf8fd49f306ee72b1314d73
Alex Chertilin <chertilin@yahoo.com>
2021-11-19 00:00:55 +0300

    feat: реализовать метод RigidBody2D.AddExplosionForce

---
+++ project/Assets/TheGame/Core/Common/Utils/Scripts/Rigidbody2DExtensions.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace TheGame.Core.Common.Utils
+{
+    public static class Rigidbody2DExtensions
+    {
+        /// <summary>
+        ///   Логика скопирована из https://stackoverflow.com/a/34252052
+        /// </summary>
+        public static void AddExplosionForce(this Rigidbody2D body, float force, Vector2 position,
+            float upwardsModifier = 0.0F, ForceMode2D mode = ForceMode2D.Force)
+        {
+            Vector2 direction = body.position - position;
+            float distance = direction.magnitude;
+
+            // Normalize without computing magnitude again
+            if (upwardsModifier == 0)
+                direction /= distance;
+            else
+            {
+                // From Rigidbody.AddExplosionForce doc:
+                // If you pass a non-zero value for the upwardsModifier parameter, the direction
+                // will be modified by subtracting that value from the Y component of the centre point.
+                direction.y += upwardsModifier;
+                direction.Normalize();
+            }
+
+            body.AddForce(Mathf.Lerp(0, force, 1 - distance) * direction, mode);
+        }
+    }
+}
