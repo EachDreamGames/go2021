bf2de009a688874e3ab40c6db3d6e1f94c0ee361
Sergey Bugay <SeregaAltmer@ya.ru>
2021-11-18 17:48:20 +0300

    fix: устранить непредумышленное прилипание персонажа к стенам

--- project/Assets/TheGame/Characters/Ladybug/Scripts/LadybugMovement.cs
+++ project/Assets/TheGame/Characters/Ladybug/Scripts/LadybugMovement.cs
@@ -7,13 +7,18 @@ namespace TheGame.Characters.Ladybug
   public class LadybugMovement : AnimatorStateAttributeBehaviour
   {
     [SerializeField] private Rigidbody2D _body;
-    [SerializeField] private IntAnimatorParameter _direction;
-    [SerializeField] private FloatAnimatorParameter _speed;
+    [SerializeField] private IntAnimatorParameter _directionParameter;
+    [SerializeField] private FloatAnimatorParameter _speedParameter;
+    [SerializeField] private BoolAnimatorParameter _isWallTouchingParameter;
 
     protected override void OnStateDeactivate() =>
       _body.velocity = new Vector2(0, _body.velocity.y);
 
-    protected override void OnStateUpdate() =>
-      _body.velocity = new Vector2(Animator.GetValue(_speed) * Animator.GetValue(_direction), _body.velocity.y);
+    protected override void OnStateUpdate()
+    {
+      if (Animator.GetValue(_isWallTouchingParameter)) return;
+
+      _body.velocity = new Vector2(Animator.GetValue(_speedParameter) * Animator.GetValue(_directionParameter), _body.velocity.y);
+    }
   }
 }
---
+++ project/Assets/TheGame/Characters/Ladybug/Scripts/LadybugWallCheck.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using TheGame.Core.Animations.Parameters;
+using UnityEngine;
+
+namespace TheGame.Characters.Ladybug
+{
+  public class LadybugWallCheck : MonoBehaviour
+  {
+    [SerializeField] private Animator _animator;
+    [SerializeField] private BoolAnimatorParameter _isWallTouchingParam;
+    [SerializeField] private LayerMask _groundLayers;
+    [SerializeField] private Collider2D _collider;
+    [SerializeField] private float _yPadding = 0.1f;
+    [SerializeField] private float _xSize = 0.1f;
+
+    private readonly Collider2D[] _overlappedColliders = new Collider2D[10];
+    private bool _isDestroying;
+
+    private void Start() =>
+      StartCoroutine(DoCheck());
+
+    private void OnDestroy() =>
+      _isDestroying = true;
+
+    private IEnumerator DoCheck()
+    {
+      while (!_isDestroying)
+      {
+        yield return new WaitForFixedUpdate();
+
+        OverlapBoxInfo overlapBox = GetOverlapBoxInfo();
+
+        int overlapCount = Physics2D.OverlapBoxNonAlloc(overlapBox.Center, overlapBox.Size, 0f, _overlappedColliders, _groundLayers);
+
+        bool isWallTouching = false;
+        int i = 0;
+
+        while (!isWallTouching && i < overlapCount)
+        {
+          Collider2D overlappedCollider = _overlappedColliders[i];
+          isWallTouching = !overlappedCollider.usedByEffector || !overlappedCollider.GetComponent<PlatformEffector2D>();
+          i++;
+        }
+
+        _animator.SetValue(_isWallTouchingParam, isWallTouching);
+      }
+    }
+
+    private OverlapBoxInfo GetOverlapBoxInfo()
+    {
+      Bounds bounds = _collider.bounds;
+      float x = transform.lossyScale.x > 0 ? bounds.max.x : bounds.min.x;
+      float maxY = bounds.max.y - _yPadding;
+      float minY = bounds.min.y + _yPadding;
+      return new OverlapBoxInfo
+      {
+        Center = new Vector2(x, (maxY + minY) / 2),
+        Size = new Vector2(_xSize, maxY - minY)
+      };
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmosSelected()
+    {
+      OverlapBoxInfo overlapBox = GetOverlapBoxInfo();
+      
+      Gizmos.color = !Application.isPlaying ? Color.white : _animator.GetValue(_isWallTouchingParam) ? Color.green : Color.red;
+      Gizmos.DrawCube(overlapBox.Center, overlapBox.Size);
+    }
+#endif
+
+    private struct OverlapBoxInfo
+    {
+      public Vector2 Center;
+      public Vector2 Size;
+    }
+  }
+}
