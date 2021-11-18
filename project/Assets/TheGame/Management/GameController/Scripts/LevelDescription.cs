f92b332770b2fc78b1839780fbac12c9fefaec3b
Sergey Bugay <SeregaAltmer@ya.ru>
2021-11-18 14:09:39 +0300

    feat: реализовать описатели уровней
    Описатели позволят не использовать строки для того чтобы ссылаться на
    сцены нужных уровней.

---
+++ project/Assets/TheGame/Management/GameController/Scripts/LevelDescription.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace TheGame.Management.GameController
+{
+  [CreateAssetMenu(fileName = "NewLevelDescription", menuName = "LevelDescription")]
+  public class LevelDescription : ScriptableObject
+  {
+    [SerializeField] private string _sceneName;
+
+    public string SceneName => _sceneName;
+  }
+}
