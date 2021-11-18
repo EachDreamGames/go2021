421c49c222a76bd7b392dca80c554579c1ee6dc3
Alex Chertilin <chertilin@yahoo.com>
2021-11-19 00:01:41 +0300

    feat: реализовать удар ногой
    Мяч настроен и добавлен на сцену

---
+++ project/Assets/TheGame/Characters/Ladybug/Scripts/LadybugKick.cs
@@ -0,0 +1,39 @@
+﻿using TheGame.Core.Animations.Attributes;
+using TheGame.Core.Common.Utils;
+using UnityEngine;
+
+namespace TheGame.Characters.Ladybug
+{
+    public class LadybugKick : AnimatorStateAttributeBehaviour
+    {
+        [SerializeField] private LayerMask _kickableLayers;
+        [SerializeField] private LadybugKickAnimationsEvents _animationsEvents;
+        [SerializeField] private Collider2D _kickArea;
+        [SerializeField] private Transform _kickPoint;
+        [SerializeField] private float _force;
+        [SerializeField] private float _upwardsModifier;
+
+        private readonly Collider2D[] _overlappedColliders = new Collider2D[5];
+
+        private void Start() =>
+            _animationsEvents.OnKick += DoKick;
+
+        private void OnDestroy() =>
+            _animationsEvents.OnKick -= DoKick;
+
+        private void DoKick()
+        {
+            int overlapCount = _kickArea.OverlapCollider(new ContactFilter2D().NoFilter(), _overlappedColliders);
+            if (overlapCount == 0) return;
+
+            for (int i = 0; i < overlapCount; i++)
+            {
+                Rigidbody2D body = _overlappedColliders[i].GetComponent<Rigidbody2D>();
+                if (body == null) continue;
+                if (_kickableLayers != (_kickableLayers | (1 << body.gameObject.layer))) continue;
+
+                body.AddExplosionForce(_force, _kickPoint.position, _upwardsModifier, ForceMode2D.Impulse);
+            }
+        }
+    }
+}
---
+++ project/Assets/TheGame/Characters/Ladybug/Scripts/LadybugKickAnimationsEvents.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace TheGame.Characters.Ladybug
+{
+    public class LadybugKickAnimationsEvents : MonoBehaviour
+    {
+        public event Action OnKick;
+        
+        public void InvokeOnKickEvent() => 
+            OnKick?.Invoke();
+    }
+}
