5884ed648493c80261d0aeeaf13c8f843d04e7d8
Sergey Bugay <SeregaAltmer@ya.ru>
2021-11-23 23:24:29 +0300

    feat: реализовать компонент позволяющий уничтожать игровые объекты

---
+++ project/Assets/TheGame/Core/Common/Scripts/GameObjectDestroyer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace TheGame.Core.Common
+{
+  
+  
+  public class GameObjectDestroyer : MonoBehaviour
+  {
+    [SerializeField] private GameObject _gameObject;
+
+    public void DestroyGameObject() => 
+      Destroy(_gameObject);
+  }
+}
