e257a4bcbc508753560692c18839d50a85f41884
Sergey Bugay <SeregaAltmer@ya.ru>
2021-11-25 19:58:47 +0300

    feat: реализовать компоненты позволяющие ссылаться на точки сцены из префабов

---
+++ project/Assets/TheGame/Common/Scripts/SceneAnchor.cs
@@ -0,0 +1,12 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace TheGame.Common
+{
+  [AssetSelector]
+  [CreateAssetMenu(fileName = "NewSceneAnchor", menuName = "SceneAnchor")]
+  public class SceneAnchor : ScriptableObject
+  {
+    [ReadOnly] public Transform Transform;
+  }
+}
---
+++ project/Assets/TheGame/Common/Scripts/SceneAnchorInformer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace TheGame.Common
+{
+  public class SceneAnchorInformer : MonoBehaviour
+  {
+    [SerializeField] private SceneAnchor _anchor;
+    [SerializeField] private Transform _transform;
+
+    private void OnEnable() =>
+      _anchor.Transform = _transform;
+
+    private void OnDisable()
+    {
+      if (_anchor.Transform == _transform)
+        _anchor.Transform = null;
+    }
+  }
+}
