8dee5787374951d915a1dda6d18bf107c6b9aa11
Sergey Bugay <SeregaAltmer@ya.ru>
2021-11-26 11:04:29 +0300

    feat: реализовать охлаждение для атаки босса

---
+++ project/Assets/TheGame/Characters/FinalBoss/Scripts/FinalBossAttackCooldown.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using Sirenix.OdinInspector;
+using TheGame.Core.Animations.Attributes;
+using TheGame.Core.Animations.Parameters;
+using UnityEngine;
+
+namespace TheGame.Characters.FinalBoss.Scripts
+{
+  public class FinalBossAttackCooldown : AnimatorStateAttributeBehaviour
+  {
+    [SerializeField] private BoolAnimatorParameter _isAttackCoolingParameter;
+    [SerializeField, SuffixLabel("s")] private float _timer;
+
+    protected override void OnStateActivate() =>
+      StartCooling();
+
+    protected override void OnStateDeactivate() =>
+      StartCoroutine(StopCooling());
+
+    private void StartCooling() =>
+      Animator.SetValue(_isAttackCoolingParameter, true);
+
+    private IEnumerator StopCooling()
+    {
+      yield return new WaitForSeconds(_timer);
+      Animator.SetValue(_isAttackCoolingParameter, false);
+    }
+  }
+}
