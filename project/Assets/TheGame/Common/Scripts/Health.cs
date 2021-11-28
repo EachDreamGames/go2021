ab00bdc04e05776cbe3b5a4f9772660f846d20ae
Sergey Bugay <SeregaAltmer@ya.ru>
2021-11-28 19:28:09 +0300

    feat: реализовать компонент здоровья

---
+++ project/Assets/TheGame/Common/Scripts/Health.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace TheGame.Common
+{
+  public class Health : MonoBehaviour
+  {
+    [SerializeField, MinValue(0)] private int _maxValue;
+
+    [SerializeField, PropertyRange(0, "$" + nameof(_maxValue))]
+    private int _currentValue;
+
+    [SerializeField] private IntUnityEvent _onModified;
+
+    public void Modify(int delta)
+    {
+      int newValue = Math.Max(0, Math.Min(_currentValue + delta, _maxValue));
+      if (_currentValue == newValue) return;
+      _currentValue = newValue;
+
+      _onModified?.Invoke(_currentValue);
+    }
+
+    public void Decrease(int value)
+    {
+      if (value <= 0) return;
+      Modify(-value);
+    }
+
+    public void Increase(int value)
+    {
+      if (value <= 0) return;
+      Modify(value);
+    }
+
+    [Serializable]
+    private class IntUnityEvent : UnityEvent<int>
+    {
+    }
+  }
+}
