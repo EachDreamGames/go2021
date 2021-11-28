f58578e9ba5d4bf79941797a9c7c8dc10d725cd7
Sergey Bugay <SeregaAltmer@ya.ru>
2021-11-28 18:20:55 +0300

    feat: добавить границы сброса уровня на второй уровень

---
+++ project/Assets/TheGame/Levels/Scripts/LevelBounds.cs
@@ -0,0 +1,14 @@
+﻿using TheGame.Management.GameController;
+using UnityEngine;
+
+namespace TheGame.Levels.Scripts
+{
+  [RequireComponent(typeof(Collider2D))]
+  public class LevelBounds : MonoBehaviour
+  {
+    [SerializeField] private LevelController _levelController;
+
+    private void OnTriggerEnter2D(Collider2D _) =>
+      _levelController.LevelRestart();
+  }
+}
