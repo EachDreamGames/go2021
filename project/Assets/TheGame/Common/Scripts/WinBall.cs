9fc58cb1db92928b24648c8715a5744ee0a4b4b5
Alex Chertilin <chertilin@yahoo.com>
2021-12-01 21:01:35 +0300

    chore: финальные штрихи
    В основном звуки и новый HappyEnd
    Для последнего изменен контроллер

--- project/Assets/TheGame/Common/Scripts/WinBall.cs
+++ project/Assets/TheGame/Common/Scripts/WinBall.cs
@@ -1,19 +1,22 @@
 ﻿using System.Collections;
 using TheGame.Management.GameController;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace TheGame.Common
 {
     public class WinBall : MonoBehaviour
     {
         [SerializeField] private LevelControllerStaticContainer _levelControllerContainer;
+        [SerializeField] private UnityEvent _onTrigger;
 
         private void OnTriggerEnter2D(Collider2D _)
         {
+            _onTrigger?.Invoke();
             StartCoroutine(DoFinishGame());
             IEnumerator DoFinishGame()
             {
-                yield return new WaitForSeconds(2f);
+                yield return new WaitForSeconds(4f);
                 _levelControllerContainer.LevelController.FinishGame();
             }
         }
---
+++ project/Assets/TheGame/Management/GameController/Scripts/HappyEnd.cs
@@ -0,0 +1,12 @@
+﻿using TheGame.Core.Animations.Attributes;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TheGame.Management.GameController
+{
+    public class HappyEnd : AnimatorStateAttributeBehaviour
+    {
+        [SerializeField] private string _sceneName;
+        protected override void OnStateActivate() => SceneManager.LoadScene(_sceneName);
+    }
+}
